b645532370dc8bba1fd78dd76f944d7b057d8d5a
enkoding1 <ksjdevelop53@gmail.com>
2025-12-08 17:21:53 +0900

    feat: ch-8.1

--- rtweekend.h
+++ rtweekend.h
@@ -1,7 +1,9 @@
 #ifndef RTWEEKEND_H
 #define RTWEEKEND_H
 
+#include <random>
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -17,6 +19,25 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
     return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generaotr;
+    return distribution(generaotr);
+}
+
+inline double random_double()
+{
+    // Returns a random real in (0, 1).
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+    // Return a random real in (min, max);
+    return min + (max - min) * random_double();
 }
 
 // Common Headers
