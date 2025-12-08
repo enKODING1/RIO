f6b1bccdc00b38f5dbd577a326b062110ea00e67
enkoding1 <ksjdevelop53@gmail.com>
2025-12-08 14:05:03 +0900

    feat: ch-6.1

--- main.cpp
+++ main.cpp
@@ -3,24 +3,35 @@
 #include "vec3.h"
 #include <iostream>
 
-bool hit_sphere(const point3 &center, double radius, const ray &r) {
+double hit_sphere(const point3 &center, double radius, const ray &r)
+{
   vec3 oc = r.origin() - center;
   auto a = dot(r.direction(), r.direction());
   auto b = 2.0 * dot(oc, r.direction());
   auto c = dot(oc, oc) - radius * radius;
   auto discriminant = b * b - 4 * a * c;
-  return (discriminant > 0);
+
+  if (discriminant < 0)
+    return -1.0;
+  else
+    return (-b - std::sqrt(discriminant)) / (2.0 * a);
 }
 
-color ray_color(const ray &r) {
-  if (hit_sphere(point3(0, 0, -1), 0.5, r))
-    return color(1, 0, 0);
+color ray_color(const ray &r)
+{
+  auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+  if (t > 0.0)
+  {
+    vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+    return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+  }
   vec3 unit_direction = unit_vector(r.direction());
   auto a = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
-int main() {
+int main()
+{
   // Image
   auto aspect_ratio = 16.0 / 9.0;
   int image_width = 400;
@@ -45,12 +56,15 @@ int main() {
   auto pixel00_loc =
       viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
   // Render
-  std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+  std::cout << "P3\n"
+            << image_width << " " << image_height << "\n255\n";
 
-  for (int j = 0; j < image_height; j++) {
+  for (int j = 0; j < image_height; j++)
+  {
     std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
               << std::flush;
-    for (int i = 0; i < image_width; i++) {
+    for (int i = 0; i < image_width; i++)
+    {
       auto pixel_center =
           pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
       auto ray_direction = pixel_center - camera_center;
